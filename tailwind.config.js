5081d04824662bf94e07723151198a1a99ea8399
Hunter <davishunterboy@gmail.com>
2024-09-05 06:44:52 -0400

    Added new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
   content: ['*.html'],
   theme: {
     
-    extend: {
-      fontFamily: {
-        "Poetsen One": ['Poetsen One','sans-serif']
-      },
-    },
+    
   },
   plugins: [],
 }
